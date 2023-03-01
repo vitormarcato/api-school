9a92160ae6ff7e356386d90cfc0aea71d4e17801
Vitor Marcato <vitor.marcatof@gmail.com>
2023-02-28 22:47:10 -0300

    refactor(EnrollmentRepository): Query changed for better performance

--- src/main/java/br/com/alura/school/enrollment/EnrollmentRepository.java
+++ src/main/java/br/com/alura/school/enrollment/EnrollmentRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     Boolean existsByCourseIdAndUserId(long courseId, Long userId);
-    @Query("SELECT new br.com.alura.school.enrollment.EnrollmentResponse(u.email, COUNT(e.course)) FROM Enrollment e LEFT JOIN e.user u WHERE (SELECT COUNT(e.course) FROM Enrollment e) > 0 GROUP BY u.email ORDER BY COUNT(e.course) DESC")
+    @Query("SELECT new br.com.alura.school.enrollment.EnrollmentResponse(u.email, COUNT(e.course)) FROM Enrollment e LEFT JOIN e.user u GROUP BY u.email ORDER BY COUNT(e.course) DESC")
     List<EnrollmentResponse> generateEnrollmentsReport();
 }
